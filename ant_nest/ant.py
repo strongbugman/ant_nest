bbd06331c9ba8a2476f76ded5c114ee2bf3d20c4
bugman <1wumingyu1@gmail.com>
2017-12-04 23:01:12 +0800

    DEV: Reuse client session(in aiohttp);add timeout for coroutines default;handle exceptions in custom "ensure_futures" method; fix errors

--- ant_nest/ant.py
+++ ant_nest/ant.py
@@ -1,13 +1,15 @@
-from typing import Any, Optional, Iterator, List, Coroutine, Union
+from typing import Any, Optional, Iterator, List, Coroutine, Union, Dict
 import abc
 import itertools
 import logging
 import asyncio
 from asyncio.queues import Queue, QueueEmpty
 from itertools import islice
+import async_timeout
 
 import aiohttp
 from aiohttp.client_reqrep import ClientResponse
+from aiohttp import ClientSession
 from yarl import URL
 
 from .pipelines import Pipeline
@@ -15,51 +17,61 @@ from .things import Request, Response, Item, Things
 from .exceptions import ThingDropped
 
 
+DEFAULT_VALUE = -1
+
+
 class Ant(abc.ABC):
     response_pipelines = [Pipeline()]  # type: List[Pipeline]
     request_pipelines = [Pipeline()]  # type: List[Pipeline]
     item_pipelines = [Pipeline()]  # type: List[Pipeline]
 
+    CONCURRENT_LIMIT = 30  # backgroud coroutines concurrent limit
+    COROUTINE_TIMEOUT = 180
+
     def __init__(self):
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.sessions = {}  # type: Dict[str, ClientSession]
         # for background coroutines - coroutines schedule by self.ensure_future method
         self.__queue = Queue()  # background coroutines waiting for execution
         self.__done_queue = Queue()  # execution completed background coroutines
         self.__count = 0  # backgroud coroutines be executed
         self.__done_count = 0  # backgroud coroutines completed
-        self.__limit = 1000
 
-    async def request(self, url: Union[str, URL], method='GET', params: Optional[dict]=None, headers: Optional[dict]=None,
-                      cookies: Optional[dict]=None, data: Optional[Any]=None,
-                      proxy: Optional[str]=None) -> Response:
+    async def request(self, url: Union[str, URL], method='GET', params: Optional[dict]=None,
+                      headers: Optional[dict]=None, cookies: Optional[dict]=None, data: Optional[Any]=None,
+                      proxy: Optional[str]=None, timeout: int=DEFAULT_VALUE) -> Response:
         self.logger.debug('{:s} {:s}'.format(method, str(url)))
         kwargs = locals()
         kwargs.pop('self')
+        timeout = kwargs.pop('timeout')
 
         req = Request(**kwargs)
-        req = await self._handle_thing_with_pipelines(req, self.request_pipelines)
+        req = await self.timeout_wrapper(self._handle_thing_with_pipelines(req, self.request_pipelines), timeout=timeout)
 
-        res = await self._request(req)
+        res = await self.timeout_wrapper(self._request(req), timeout=timeout)
 
-        res = await self._handle_thing_with_pipelines(res, self.response_pipelines)
+        res = await self.timeout_wrapper(self._handle_thing_with_pipelines(res, self.response_pipelines),
+                                         timeout=timeout)
         return res
 
-    async def collect(self, item: Item) -> None:
+    async def collect(self, item: Item, timeout: int=DEFAULT_VALUE) -> None:
         self.logger.debug('Collect item: ' + str(item))
-        await self._handle_thing_with_pipelines(item, self.item_pipelines)
+        await self.timeout_wrapper(self._handle_thing_with_pipelines(item, self.item_pipelines), timeout=timeout)
 
-    async def open(self) -> None:
+    async def open(self, timeout: int=DEFAULT_VALUE) -> None:
         self.logger.info('Opening')
         for pipeline in itertools.chain(self.item_pipelines, self.response_pipelines, self.request_pipelines):
             obj = pipeline.on_spider_open(self)
             if isinstance(obj, Coroutine):
-                await obj
+                await self.timeout_wrapper(obj, timeout=timeout)
 
-    async def close(self) -> None:
+    async def close(self, timeout: int=DEFAULT_VALUE) -> None:
         for pipeline in itertools.chain(self.item_pipelines, self.response_pipelines, self.request_pipelines):
             obj = pipeline.on_spider_close(self)
             if isinstance(obj, Coroutine):
-                await obj
+                await self.timeout_wrapper(obj, timeout=timeout)
+        for session in self.sessions.values():
+            await session.close()
         self.logger.info('Closed')
 
     @abc.abstractmethod
@@ -75,15 +87,24 @@ class Ant(abc.ABC):
         except Exception as e:
             self.logger.exception('Run main coroutine with ' + e.__class__.__name__)
 
-    def ensure_future(self, coroutine: Coroutine) -> None:
-        """Custom ensure_future method provide coroutines limit and watch dog"""
+    def ensure_future(self, coroutine: Coroutine, timeout: int=DEFAULT_VALUE) -> None:
+        """Custom ensure_future method provide coroutines concurrent limit and watch dog"""
+        if timeout == DEFAULT_VALUE:
+            timeout = self.COROUTINE_TIMEOUT
 
         def _done_callback(f):
+            exception = f.exception()
+            if exception is not None:
+                try:
+                    raise exception
+                except exception.__class__:
+                    self.logger.exception(exception)
+
             self.__done_count += 1
             self.__done_queue.put_nowait(f)
             try:
                 running_count = self.__count - self.__done_count
-                if running_count < self.__limit:
+                if running_count < self.CONCURRENT_LIMIT:
                     next_coroutine = self.__queue.get_nowait()
                     self.__count += 1
                     asyncio.ensure_future(next_coroutine).add_done_callback(_done_callback)
@@ -91,19 +112,22 @@ class Ant(abc.ABC):
                 pass
 
         running_count = self.__count - self.__done_count
-        if running_count < self.__limit:
+        if running_count < self.CONCURRENT_LIMIT:
             self.__count += 1
-            asyncio.ensure_future(coroutine).add_done_callback(_done_callback)
+            asyncio.ensure_future(self.timeout_wrapper(coroutine, timeout=timeout)).add_done_callback(_done_callback)
         else:
-            self.__queue.put_nowait(coroutine)
+            self.__queue.put_nowait(self.timeout_wrapper(coroutine, timeout=timeout))
 
     def as_completed(self, coroutines: Union[Iterator[Coroutine], List],
-                     limit: int=-1) -> Iterator[Coroutine]:
-        """Custom as_completed method provide coroutines limit"""
-        if limit == -1:
-            limit = self.__limit
+                     limit: int=DEFAULT_VALUE, timeout: int=DEFAULT_VALUE) -> Iterator[Coroutine]:
+        """Custom as_completed method provide coroutines concurrent limit"""
+        if limit == DEFAULT_VALUE:
+            limit = self.CONCURRENT_LIMIT
+        if timeout == DEFAULT_VALUE:
+            timeout = self.COROUTINE_TIMEOUT
+
         if isinstance(coroutines, List):
-            coroutines = Iterator(coroutines)
+            coroutines = iter(coroutines)
 
         queue = Queue()
         todo = []
@@ -123,15 +147,25 @@ class Ant(abc.ABC):
             return f.result()
 
         if limit <= 0:
-            fs = {asyncio.ensure_future(cor) for cor in coroutines}
+            fs = {asyncio.ensure_future(self.timeout_wrapper(cor, timeout=timeout)) for cor in coroutines}
         else:
-            fs = {asyncio.ensure_future(cor) for cor in islice(coroutines, 0, limit)}
+            fs = {asyncio.ensure_future(self.timeout_wrapper(cor, timeout=timeout))
+                  for cor in islice(coroutines, 0, limit)}
         for f in fs:
             f.add_done_callback(_done_callback)
             todo.append(f)
 
         while len(todo) > 0:
             yield _wait_for_one()
+
+    async def timeout_wrapper(self, coroutine: Coroutine, timeout: int=DEFAULT_VALUE):
+        if timeout == DEFAULT_VALUE:
+            timeout = self.COROUTINE_TIMEOUT
+        if timeout > 0:
+            with async_timeout.timeout(timeout):
+                return await coroutine
+        else:
+            return await coroutine
 
     async def __run_until_complete(self) -> None:
         """Watch dog,  wait all background coroutines to be completed"""
@@ -157,12 +191,22 @@ class Ant(abc.ABC):
     async def _request(self, req: Request) -> Response:
         kwargs = {k: getattr(req, k) for k in req.__slots__}
         cookies = kwargs.pop('cookies')
-        async with aiohttp.ClientSession(cookies=cookies) as session:
-            async with session.request(**kwargs) as aio_response:
-                await aio_response.read()
+
+        host = req.url.host
+        if host not in self.sessions:
+            session = aiohttp.ClientSession(cookies=cookies)
+            self.sessions[host] = session
+        else:
+            session = self.sessions[host]
+            if cookies is not None:
+                session.cookie_jar.update_cookies(cookies)
+
+        async with session.request(**kwargs) as aio_response:
+            await aio_response.read()
         return self._convert_response(aio_response, req)
 
-    def _convert_response(self, aio_response: ClientResponse, request: Request) -> Response:
+    @staticmethod
+    def _convert_response(aio_response: ClientResponse, request: Request) -> Response:
         return Response(request, aio_response.status, aio_response._content,
                         headers=aio_response.headers, cookies=aio_response.cookies,
                         encoding=aio_response._get_encoding())
