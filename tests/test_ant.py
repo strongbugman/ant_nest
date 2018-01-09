bcf93a3bfba3dd17a1e526921f8a8f1f4debbd61
bugman <1wumingyu1@gmail.com>
2018-01-09 22:41:05 +0800

    Add proxy test

--- tests/test_ant.py
+++ tests/test_ant.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from multidict import CIMultiDictProxy, CIMultiDict
 
 import pytest
@@ -152,7 +153,7 @@ async def test_pipelines():
 
 @pytest.mark.asyncio
 async def test_with_real_request():
-    httpbin_base_url = 'http://localhost:8080/'
+    httpbin_base_url = os.getenv('TEST_HTTPBIN', 'http://localhost:8080/')
 
     class TestAnt(Ant):
         async def run(self):
@@ -214,6 +215,11 @@ async def test_with_real_request():
     res = await ant.request(httpbin_base_url + 'redirect/3')
     assert res.status == 302
     assert ant._reports['Request'][1] > 12
+    # with proxy
+    proxy = os.getenv('TEST_PROXY', 'http://localhost:3128')
+    ant.request_proxy = proxy
+    res = await ant.request('http://httpbin.org/anything')
+    assert res.status == 200
     # dropped item report
     ant.report(Item(), dropped=True)
     await ant.main()
