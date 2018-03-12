99d6bd0e4e7231b7b0aa90616339b52fd9e98cdb
strongbugman <strongbugman@gmail.com>
2018-03-12 17:46:57 +0800

    Add "ExceptionFilter";

--- ant_nest/__init__.py
+++ ant_nest/__init__.py
@@ -1,49 +1,13 @@
-from typing import Any
-
 from .ant import *
 from .things import *
 from .pipelines import *
 from .coroutine_pool import *
 from .exceptions import *
+from .utils import *
 
 
 __version__ = '0.32.0'
 
 
-def extract_value(_type: str, path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                  ignore_exception: bool=True,
-                  default: Any=None) -> Any:
-    try:
-        return ItemExtractor.extract_value(_type, path, data, extract_type=extract_type)
-    except Exception as e:
-        if ignore_exception:
-            return default
-        else:
-            raise e
-
-
-def extract_value_by_xpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('xpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
-                         default=default)
-
-
-def extract_value_by_jpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('jpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
-                         default=default)
-
-
-def extract_value_by_regex(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('regex', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
-                         default=default)
-
-
-class CliAnt(Ant):
-    async def run(self):
-        pass
-
-
 __all__ = ant.__all__ + pipelines.__all__ + exceptions.__all__ + coroutine_pool.__all__ + \
-          things.__all__ + ['extract_value_by_jpath', 'extract_value_by_regex', 'extract_value_by_xpath']
+          things.__all__ + utils.__all__
---
+++ ant_nest/utils.py
@@ -0,0 +1,59 @@
+"""Utilities box"""
+from typing import Any, List, Type
+import logging
+
+from .ant import Ant
+from .things import ItemExtractor
+
+
+__all__ = ['CliAnt', 'extract_value_by_xpath', 'extract_value_by_jpath', 'extract_value_by_regex', 'ExceptionFilter']
+
+
+class CliAnt(Ant):
+    async def run(self):
+        pass
+
+
+def extract_value(_type: str, path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
+                  ignore_exception: bool=True,
+                  default: Any=None) -> Any:
+    try:
+        return ItemExtractor.extract_value(_type, path, data, extract_type=extract_type)
+    except Exception as e:
+        if ignore_exception:
+            return default
+        else:
+            raise e
+
+
+def extract_value_by_xpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
+                           ignore_exception: bool=True, default: Any=None) -> Any:
+    return extract_value('xpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+                         default=default)
+
+
+def extract_value_by_jpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
+                           ignore_exception: bool=True, default: Any=None) -> Any:
+    return extract_value('jpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+                         default=default)
+
+
+def extract_value_by_regex(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
+                           ignore_exception: bool=True, default: Any=None) -> Any:
+    return extract_value('regex', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+                         default=default)
+
+
+class ExceptionFilter(logging.Filter):
+    """A exception log filter class for logging.
+    """
+    def __init__(self, exceptions: List[Type[Exception]], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.exceptions = exceptions
+
+    def filter(self, record):
+        if record.exc_info:
+            for e in self.exceptions:
+                if record.exc_info[0] is e:
+                    return False
+        return True
