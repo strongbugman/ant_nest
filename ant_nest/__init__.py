05da7c94d31db0c3def21ae83333a0295b6ac530
strongbugman <strongbugman@gmail.com>
2018-03-23 14:21:51 +0800

    Add flake8 check

--- ant_nest/__init__.py
+++ ant_nest/__init__.py
@@ -1,14 +1,12 @@
-from .ant import *
-from .things import *
-from .pipelines import *
-from .coroutine_pool import *
-from .exceptions import *
-from .utils import *
-from .utils import CliAnt
-
+from .ant import *  # noqa
+from .things import *  # noqa
+from .pipelines import *  # noqa
+from .coroutine_pool import *  # noqa
+from .exceptions import *  # noqa
+from .utils import *  # noqa
+from .utils import CliAnt  # noqa
 
 __version__ = '0.33.0'
 
-
-__all__ = ant.__all__ + pipelines.__all__ + exceptions.__all__ + coroutine_pool.__all__ + \
-          things.__all__ + utils.__all__
+__all__ = (ant.__all__ + pipelines.__all__ + exceptions.__all__ +  # noqa
+           coroutine_pool.__all__ + things.__all__ + utils.__all__)  # noqa
--- ant_nest/utils.py
+++ ant_nest/utils.py
@@ -5,8 +5,8 @@ import logging
 from .ant import Ant
 from .things import ItemExtractor
 
-
-__all__ = ['extract_value_by_xpath', 'extract_value_by_jpath', 'extract_value_by_regex', 'ExceptionFilter']
+__all__ = ['extract_value_by_xpath', 'extract_value_by_jpath',
+           'extract_value_by_regex', 'ExceptionFilter']
 
 
 class CliAnt(Ant):
@@ -14,11 +14,13 @@ class CliAnt(Ant):
         pass
 
 
-def extract_value(_type: str, path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                  ignore_exception: bool=True,
-                  default: Any=None) -> Any:
+def extract_value(_type: str, path: str, data: Any,
+                  extract_type: str = ItemExtractor.extract_with_take_first,
+                  ignore_exception: bool = True,
+                  default: Any = None) -> Any:
     try:
-        return ItemExtractor.extract_value(_type, path, data, extract_type=extract_type)
+        return ItemExtractor.extract_value(_type, path, data,
+                                           extract_type=extract_type)
     except Exception as e:
         if ignore_exception:
             return default
@@ -26,27 +28,40 @@ def extract_value(_type: str, path: str, data: Any, extract_type: str=ItemExtrac
             raise e
 
 
-def extract_value_by_xpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('xpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+def extract_value_by_xpath(
+        path: str, data: Any,
+        extract_type: str = ItemExtractor.extract_with_take_first,
+        ignore_exception: bool = True,
+        default: Any = None) -> Any:
+    return extract_value('xpath', path, data, extract_type=extract_type,
+                         ignore_exception=ignore_exception,
                          default=default)
 
 
-def extract_value_by_jpath(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('jpath', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+def extract_value_by_jpath(
+        path: str, data: Any,
+        extract_type: str = ItemExtractor.extract_with_take_first,
+        ignore_exception: bool = True,
+        default: Any = None) -> Any:
+    return extract_value('jpath', path, data, extract_type=extract_type,
+                         ignore_exception=ignore_exception,
                          default=default)
 
 
-def extract_value_by_regex(path: str, data: Any, extract_type: str=ItemExtractor.extract_with_take_first,
-                           ignore_exception: bool=True, default: Any=None) -> Any:
-    return extract_value('regex', path, data, extract_type=extract_type, ignore_exception=ignore_exception,
+def extract_value_by_regex(
+        path: str, data: Any,
+        extract_type: str = ItemExtractor.extract_with_take_first,
+        ignore_exception: bool = True,
+        default: Any = None) -> Any:
+    return extract_value('regex', path, data, extract_type=extract_type,
+                         ignore_exception=ignore_exception,
                          default=default)
 
 
 class ExceptionFilter(logging.Filter):
     """A exception log filter class for logging.
     """
+
     def __init__(self, exceptions: List[Type[Exception]], *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.exceptions = exceptions
