7e2dd10182033f0c00cc23c697e04a7246665fee
strongbugman <strongbugman@gmail.com>
2018-06-08 18:13:58 +0800

    Remove timeout support

--- ant_nest/things.py
+++ ant_nest/things.py
@@ -8,7 +8,7 @@ import logging
 import re
 
 from typing_extensions import Protocol
-from aiohttp import ClientResponse, ClientRequest
+from aiohttp import ClientResponse, ClientRequest, sentinel
 from lxml import html
 import jpath
 import ujson
@@ -17,10 +17,11 @@ from .exceptions import FieldValidationError, ItemExtractError
 
 
 class Request(ClientRequest):
-    def __init__(self, *args, response_in_stream: bool = False,
-                 **kwargs) -> None:
+    def __init__(self, *args, timeout: float = sentinel,
+                 response_in_stream: bool = False, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.response_in_stream = response_in_stream
+        self.timeout = timeout
         # store data obj
         self.data: Optional[Union[AnyStr, dict, IO]] = kwargs.get('data', None)
 
--- ant_nest/utils.py
+++ ant_nest/utils.py
@@ -9,6 +9,28 @@ __all__ = ['extract_value_by_xpath', 'extract_value_by_jpath',
            'extract_value_by_regex', 'ExceptionFilter']
 
 
+def timeout_wrapper(
+        coro_or_func: typing.Union[typing.Coroutine, typing.Callable],
+        timeout: typing.Union[float, int]
+) -> typing.Union[typing.Coroutine, typing.Callable]:
+    """Add timeout limit to coroutine or coroutine function"""
+    is_coroutinefunction = asyncio.iscoroutinefunction(coro_or_func)
+
+    async def wrapper(*args, **kwargs):
+        with async_timeout.timeout(timeout):
+            if is_coroutinefunction:
+                return await coro_or_func(*args, **kwargs)
+            else:
+                return await coro_or_func
+
+    if timeout < 0:
+        return coro_or_func
+    elif is_coroutinefunction:
+        return wrapper
+    else:
+        return wrapper()
+
+
 class CliAnt(Ant):
     async def run(self):
         pass
