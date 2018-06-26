283a9684b2338a8fb01e71c8fa5ed6c5409c059c
strongbugman <strongbugman@gmail.com>
2018-06-26 18:40:40 +0800

    Add examples

---
+++ examples/settings.py
@@ -0,0 +1,10 @@
+"""setting module for your project"""
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# your ant`s class modules or packages
+ANT_PACKAGES = ['ants']
+
+# custom setting, eg:
+# MYSQL_HOST = '127.0.0.1'
