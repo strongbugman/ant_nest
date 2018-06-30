bce95e0e0128090a514a9c20695c1b43bdcaddae
strongbugman <strongbugman@gmail.com>
2018-06-30 19:43:17 +0800

    Add more config in setting example file

--- ant_nest/_settings_example.py
+++ ant_nest/_settings_example.py
@@ -1,10 +1,19 @@
 """setting module for your project"""
+import os
 import logging
 
-logging.basicConfig(level=logging.INFO)
+from ant_nest import ExceptionFilter
 
 # your ant`s class modules or packages
 ANT_PACKAGES = ['ants']
+ANT_ENV = os.getenv('ANT_ENV', 'development')
+
+if ANT_ENV in ('development', 'testing'):
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger().addFilter(ExceptionFilter())
+
 
 # custom setting, eg:
 # MYSQL_HOST = '127.0.0.1'
