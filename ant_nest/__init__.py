055e5b305c44e3a9d51a71bd002239ed6948e589
strongbugman <strongbugman@gmail.com>
2018-07-03 17:32:32 +0800

    Bump to 0.36!

--- ant_nest/__init__.py
+++ ant_nest/__init__.py
@@ -4,7 +4,7 @@ from .pipelines import *  # noqa
 from .exceptions import *  # noqa
 from .utils import *  # noqa
 
-__version__ = '0.35'
+__version__ = '0.36'
 
 __all__ = (ant.__all__ + pipelines.__all__ + exceptions.__all__ +  # noqa
            things.__all__ + utils.__all__)  # noqa
