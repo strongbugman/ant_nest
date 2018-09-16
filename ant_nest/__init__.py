6f7e00036865194da8f360cb9d9de84d0c4db3f9
strongbugman <strongbugman@gmail.com>
2018-09-16 20:40:58 +0800

    Up to 0.37.1!

--- ant_nest/__init__.py
+++ ant_nest/__init__.py
@@ -4,7 +4,7 @@ from .pipelines import *  # noqa
 from .exceptions import *  # noqa
 from .utils import *  # noqa
 
-__version__ = '0.36.3'
+__version__ = '0.37.1'
 
 __all__ = (['Ant'] + pipelines.__all__ + exceptions.__all__ +  # noqa
            things.__all__ + utils.__all__)  # noqa
